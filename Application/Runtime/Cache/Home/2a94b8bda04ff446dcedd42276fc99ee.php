ce48b8ff9c1caf5e50943048d071d70d5463d7a9
stone-jin <jb1520006273@gmail.com>
2015-04-15 22:00:32 +0800

    Bug ID: 00000 Description: 模仿OPPO官网，并且需要处理好各个浏览器的兼容性问题

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,19 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>OPPO手机官网-OPPO智能手机 OPPO最新款智能手机在线订购</title>
+	<link rel="stylesheet" type="text/css" href="/web_oppo/Public/Home/css/index.css">
+</head>
+<body>
+	<div class="index_header">
+		
+	</div>
+	<div class="index_header2">
+		
+	</div>
+	<div class="index_third">
+		
+	</div>
+</body>
+</html>
