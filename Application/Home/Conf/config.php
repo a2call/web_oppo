b996e0d56f1991e25c3f37e5d40a78ed244e3e0e
stone-jin <jb1520006273@gmail.com>
2015-04-24 23:30:26 +0800

    Description: 首页去掉了图片一下加载的情况，因为那样加载速度很慢的，所以要学习下类似的库，实现异步加载

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -9,8 +9,8 @@ return array(
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'web_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
-    'SHOW_PAGE_TRACE' =>true, 
-    'LOG_RECORD' => true, // 开启日志记录
-    'LOG_LEVEL'  =>'EMERG,ALERT,CRIT,ERR', // 只记录EMERG ALERT CRIT ERR 错误
-    'LOG_TYPE'              =>  'File', // 日志记录类型 默认为文件方式
+    //'SHOW_PAGE_TRACE' =>true, 
+    //'LOG_RECORD' => true, // 开启日志记录
+    //'LOG_LEVEL'  =>'EMERG,ALERT,CRIT,ERR', // 只记录EMERG ALERT CRIT ERR 错误
+    //'LOG_TYPE'              =>  'File', // 日志记录类型 默认为文件方式
 );
